2b954f9073db78e2725b2ebe702bc955db0f560e
JIBON <jibon2230@gmail.com>
2022-11-24 02:25:46 +0600

    post method added for users info

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -9,6 +10,58 @@ require('dotenv').config();
 //middlewares
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ebaxxgs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+
+
+    try {
+
+        const userCollection = client.db("VINTAGE-RESALE-MARKET").collection("user");
+
+        app.post('/users', async (req, res) => {
+
+
+            const users = req.body
+            const result = await userCollection.insertOne(users)
+            res.send(result)
+
+
+
+
+        })
+
+
+    }
+
+    finally {
+
+
+
+    }
+
+
+
+}
+run().catch(err => {
+
+    console.log(err)
+
+
+})
+
+
+
+
+
+
+
 
 
 
